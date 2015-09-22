017f55e16e39b50fcc6418f48078302680f2d39a
Joost Kemink <joost.kemink@climate.com>
2015-09-22 19:14:26 +0000

    Merge pull request #1 in FDI/tracelog-ios from ~JOOST.KEMINK/tracelog-ios:feature/BASIC-2113 to master
    Squashed commit of the following:
    
    commit 8e62b32a17de8f86bd7afd510e2cd4252b676ba2
    Author: Joost Kemink <joost@climate.com>
    Date:   Tue Sep 22 12:08:38 2015 -0700
    
        Remove the two test methods that didn't do anything. We need to take the test target out at some point.
    
    commit a6af68d11832908bd8bc3f3058acf7cc4e6c0889
    Author: Joost Kemink <joost@climate.com>
    Date:   Mon Sep 21 06:41:24 2015 -0700
    
        BASIC-2113: Update TraceLog to be buildable with Frameworks and update podspec
    
        STEP 2: Set deployment target for test target to 8.0 and run "pod update"
    
    commit 18709b58286f9aff2270d8a56c6173564313eeef
    Author: Joost Kemink <joost@climate.com>
    Date:   Mon Sep 21 06:31:15 2015 -0700
    
        BASIC-2113: Update TraceLog to be buildable with Frameworks and update podspec
    
        STEP 1: Manual (and Xcode-updated) changes to files before running "pod update"
    
        Podfile
        - Add Climate repo as source
        - use_frameworks!
    
        Tests.m
        - Comment out two sections that use AsyncBlock, don't know why these cause a problem, but since they are empty test statements anyway, I commented them out.
    
        project.pbxproj
        - Set TraceLog app deployment target to 8.0
    
        TraceLog-Example.xcscheme
        - Automatically generated by Xcode

--- Example/Pods/Target Support Files/Pods-TraceLog-TraceLog/Pods-TraceLog-TraceLog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TraceLog_TraceLog : NSObject
-@end
-@implementation PodsDummy_Pods_TraceLog_TraceLog
-@end
---
+++ Example/Pods/Target Support Files/TraceLog/TraceLog-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "TraceLog.h"
+#import "TLogger.h"
+
+FOUNDATION_EXPORT double TraceLogVersionNumber;
+FOUNDATION_EXPORT const unsigned char TraceLogVersionString[];
+
