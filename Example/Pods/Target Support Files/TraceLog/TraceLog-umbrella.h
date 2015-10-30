e4bd6056ecd10ebc2724c8447e83a03dfe94d12c
Tony Stone <hatter24@gmail.com>
2015-10-30 14:55:25 -0700

    Ran "pod update" to correct some issues with the pod structure.

--- Example/Pods/Target Support Files/TraceLog/TraceLog-umbrella.h
+++ Example/Pods/Target Support Files/TraceLog/TraceLog-umbrella.h
@@ -1,7 +1,6 @@
 #import <UIKit/UIKit.h>
 
 #import "TraceLog.h"
-#import "TLogger.h"
 
 FOUNDATION_EXPORT double TraceLogVersionNumber;
 FOUNDATION_EXPORT const unsigned char TraceLogVersionString[];
