b2c642ce473db5b0319ae1773084983c68139865
Tony Stone <hatter24@gmail.com>
2015-11-03 17:31:55 -0800

    Ran "pod update" to update project files for Swift additions.

--- Example/Pods/Target Support Files/TraceLog/TraceLog-umbrella.h
+++ Example/Pods/Target Support Files/TraceLog/TraceLog-umbrella.h
@@ -2,6 +2,8 @@
 
 #import "TraceLog.h"
 #import "TLogger.h"
+#import "TLogger.h"
+#import "TraceLog.h"
 
 FOUNDATION_EXPORT double TraceLogVersionNumber;
 FOUNDATION_EXPORT const unsigned char TraceLogVersionString[];
