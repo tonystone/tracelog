d9ffa7b287e3b961064915f31896db009b2b7b0d
Tony Stone <hatter24@gmail.com>
2015-11-30 15:39:02 -0800

    Adding bridging header to Core to force bridging for the Swift module.

---
+++ TraceLog/Core/TraceLog-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  TraceLog-Bridging-Header.h
+//  Pods
+//
+//  Created by Tony Stone on 11/30/15.
+//
+//
+
+#ifndef TraceLog_Bridging_Header_h
+#define TraceLog_Bridging_Header_h
+
+#import "TLLogger.h"
+
+
+#endif /* TraceLog_Bridging_Header_h */
