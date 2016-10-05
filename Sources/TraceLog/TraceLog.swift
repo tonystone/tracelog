d969a8072d931b2ac7f1b52bb1ac737c90ebd41c
Tony Stone <hatter24@gmail.com>
2016-10-05 10:22:00 -0700

    Update TraceLog.swift - Removing overloaded initialize methods replacing with one using defaults. - Updated documentation to new style.

--- Sources/TraceLog/TraceLog.swift
+++ Sources/TraceLog/TraceLog.swift
@@ -20,63 +20,51 @@
 import Swift
 import Foundation
 
-internal let ModuleLogName  = "TraceLog"
-
-public func initialize() {
-    #if !TRACELOG_DISABLED
-        Logger.intialize()
-    #endif
-}
-
-public func initialize(logWriters writers: [Writer]) {
+///
+/// Initializes TraceLog with an optional array of Writers and the Environment.
+///
+/// This call is optional but in order to read from the environment on start up,
+/// This method must be called.
+///
+/// - Parameters:
+///     - writers:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
+///     - environment: An closure or trailing closure that evailuates to the String message to log.
+///
+public
+func initialize(writers: [Writer] = [ConsoleWriter()], environment: Environment = Environment()) {
     #if DEBUG || TRACELOG_ENABLE || os(Linux)
-        Logger.intialize(writers, environment: Environment())
+        Logger.intialize(writers, environment: environment)
     #endif
 }
 
-public func initialize<T : Collection>(environment: T) where T.Iterator.Element == (key: String, value: String)  {
-    #if DEBUG || TRACELOG_ENABLE || os(Linux)
-        Logger.intialize([ConsoleWriter()], environment: Environment(environment))
-    #endif
-}
-
-public func initialize<T : Collection>(logWriters writers: [Writer], environment: T) where T.Iterator.Element == (key: String, value: String) {
-    #if DEBUG || TRACELOG_ENABLE || os(Linux)
-        Logger.intialize(writers, environment: Environment(environment))
-    #endif
-}
-
-// MARK: Public Interface
-
-/**
-    
-    logError logs a message with LogLevel Error to the LogWriters
-
-    - Parameters:
-        - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
-        - message: An closure or trailing closure that evailuates to the String message to log.
-
-    Examples:
-    ```
-        logError {
-            "Error message"
-        }
-
-        logError("MyAppName") {
-            "Error message"
-        }
- 
-        logError("MyAppName") {
-            
-            // You can create complex closures that ultimately
-            // return the String that will be logged to the
-            // log Writers.
- 
-            return "Final message String"
-        }
-    ```
-*/
-public func logError(_ tag: String? = nil, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message: @escaping () -> String) {
+///
+/// logError logs a message with LogLevel Error to the LogWriters
+///
+/// - Parameters:
+///     - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
+///     - message: An closure or trailing closure that evailuates to the String message to log.
+///
+/// Examples:
+/// ```
+///     logError {
+///         "Error message"
+///     }
+///
+///     logError("MyAppName") {
+///         "Error message"
+///     }
+///
+///     logError("MyAppName") {
+///
+///          /// You can create complex closures that ultimately
+///          /// return the String that will be logged to the
+///          /// log Writers.
+///
+///         return "Final message String"
+///     }
+/// ```
+public
+func logError(_ tag: String? = nil, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message: @escaping () -> String) {
     #if DEBUG || TRACELOG_ENABLE || os(Linux)
         let derivedTag = derivedTagIfNil(file: file, tag: tag)
         
@@ -84,36 +72,34 @@ public func logError(_ tag: String? = nil, _ file: String = #file, _ function: S
     #endif
 }
 
-
-/**
-    
-    logWarning logs a message with LogLevel Warning to the LogWriters
-
-    - Parameters:
-        - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
-        - message: An closure or trailing closure that evailuates to the String message to log.
-
-    Examples:
-    ```
-        logWarning {
-            "Warning message"
-        }
-
-        logWarning("MyAppName") {
-            "Warning message"
-        }
- 
-        logWarning("MyAppName") {
-            
-            // You can create complex closures that ultimately
-            // return the String that will be logged to the
-            // log Writers.
-            
-            return "Final message String"
-        }
-    ```
-*/
-public func logWarning(_ tag: String? = nil, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message:  @escaping () -> String) {
+///
+/// logWarning logs a message with LogLevel Warning to the LogWriters
+///
+/// - Parameters:
+///     - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
+///     - message: An closure or trailing closure that evailuates to the String message to log.
+///
+/// Examples:
+/// ```
+///     logWarning {
+///         "Warning message"
+///     }
+///
+///     logWarning("MyAppName") {
+///         "Warning message"
+///     }
+///
+///     logWarning("MyAppName") {
+///
+///         // You can create complex closures that ultimately
+///         // return the String that will be logged to the
+///         // log Writers.
+///
+///         return "Final message String"
+///     }
+/// ```
+public
+func logWarning(_ tag: String? = nil, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message:  @escaping () -> String) {
     #if DEBUG || TRACELOG_ENABLE || os(Linux)
         let derivedTag = derivedTagIfNil(file: file, tag: tag)
         
@@ -121,34 +107,33 @@ public func logWarning(_ tag: String? = nil, _ file: String = #file, _ function:
     #endif
 }
 
-/**
-    
-    logInfo logs a message with LogLevel Info to the LogWriters
-
-    - Parameters:
-        - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
-        - message: An closure or trailing closure that evailuates to the String message to log.
-
-    Examples:
-    ```
-        logInfo {
-            "Info message"
-        }
-
-        logInfo("MyAppName") {
-            "Info message"
-        }
- 
-        logInfo("MyAppName") {
-            
-            // You can create complex closures that ultimately
-            // return the String that will be logged to the
-            // log Writers.
-            
-            return "Final message String"
-    ```
-*/
-public func logInfo(_ tag: String? = nil, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message:  @escaping () -> String) {
+/// logInfo logs a message with LogLevel Info to the LogWriters
+///
+/// - Parameters:
+///     - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
+///     - message: An closure or trailing closure that evailuates to the String message to log.
+///
+/// Examples:
+/// ```
+///     logInfo {
+///         "Info message"
+///     }
+///
+///     logInfo("MyAppName") {
+///         "Info message"
+///     }
+///
+///     logInfo("MyAppName") {
+///
+///         // You can create complex closures that ultimately
+///         // return the String that will be logged to the
+///         // log Writers.
+///
+///         return "Final message String"
+/// ```
+///
+public
+func logInfo(_ tag: String? = nil, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message:  @escaping () -> String) {
     #if DEBUG || TRACELOG_ENABLE || os(Linux)
         let derivedTag = derivedTagIfNil(file: file, tag: tag)
         
@@ -156,40 +141,40 @@ public func logInfo(_ tag: String? = nil, _ file: String = #file, _ function: St
     #endif
 }
 
-/**
-    
-    logTrace logs a message with LogLevel Trace to the LogWriters
-
-    - Parameters:
-        - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
-        - level    An integer representing the trace LogLevel (i.e. 1, 2, 3, and 4.)
-        - message: An closure or trailing closure that evailuates to the String message to log.
-
-    Examples:
-    ```
-        logTrace {
-            "Trace message"
-        }
-
-        logTrace("MyAppName") {
-            "Trace message"
-        }
- 
-        logTrace("MyAppName", level: 3) {
-            "Trace message"
-        }
- 
-        logTrace("MyAppName") {
-            
-            // You can create complex closures that ultimately
-            // return the String that will be logged to the
-            // log Writers.
-            
-            return "Final message String"
-        }
-    ```
-*/
-public func logTrace(_ tag: String? = nil, level: Int = LogLevel.rawTraceLevels.lowerBound, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message: @escaping () -> String) {
+///
+/// logTrace logs a message with LogLevel Trace to the LogWriters
+///
+/// - Parameters:
+///     - tag:     A String to use as a tag to group this call to other calls related to it. If not passed or nil, the file name is used as a tag.
+///     - level    An integer representing the trace LogLevel (i.e. 1, 2, 3, and 4.)
+///     - message: An closure or trailing closure that evailuates to the String message to log.
+///
+/// Examples:
+/// ```
+///     logTrace {
+///         "Trace message"
+///     }
+///
+///     logTrace("MyAppName") {
+///         "Trace message"
+///     }
+///
+///     logTrace("MyAppName", level: 3) {
+///         "Trace message"
+///     }
+///
+///     logTrace("MyAppName") {
+///
+///         // You can create complex closures that ultimately
+///         // return the String that will be logged to the
+///         // log Writers.
+///
+///         return "Final message String"
+///     }
+/// ```
+///
+public
+func logTrace(_ tag: String? = nil, level: Int = LogLevel.rawTraceLevels.lowerBound, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message: @escaping () -> String) {
     #if DEBUG || TRACELOG_ENABLE || os(Linux)
         assert(LogLevel.rawTraceLevels.contains(level), "Invalid trace level, levels are in the range of \(LogLevel.rawTraceLevels)")
         
@@ -199,39 +184,39 @@ public func logTrace(_ tag: String? = nil, level: Int = LogLevel.rawTraceLevels.
     #endif
 }
 
-/**
-    
-    logTrace logs a message with LogLevel Trace to the LogWriters.
-
-    - Parameters:
-        - level    An integer representing the trace LogLevel (i.e. 1, 2, 3, and 4.)
-        - message: An closure or trailing closure that evailuates to the String message to log.
-
-    Examples:
-    ```
-        logTrace(1) {
-            "Trace message"
-        }
-
-        logTrace(2) {
-            "Trace message"
-        }
- 
-        logTrace(3) {
-            "Trace message"
-        }
- 
-        logTrace(4) {
-            
-            // You can create complex closures that ultimately
-            // return the String that will be logged to the
-            // log Writers.
-            
-            return "Final message String"
-        }
-    ```
-*/
-public func logTrace(_ level: Int, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message: @escaping () -> String) {
+///
+/// logTrace logs a message with LogLevel Trace to the LogWriters.
+///
+/// - Parameters:
+///     - level    An integer representing the trace LogLevel (i.e. 1, 2, 3, and 4.)
+///     - message: An closure or trailing closure that evailuates to the String message to log.
+///
+/// Examples:
+/// ```
+///     logTrace(1) {
+///         "Trace message"
+///     }
+///
+///     logTrace(2) {
+///         "Trace message"
+///     }
+///
+///     logTrace(3) {
+///         "Trace message"
+///     }
+///
+///     logTrace(4) {
+///
+///         // You can create complex closures that ultimately
+///         // return the String that will be logged to the
+///         // log Writers.
+///
+///         return "Final message String"
+///     }
+/// ```
+///
+public
+func logTrace(_ level: Int, _ file: String = #file, _ function: String = #function, _ line: Int = #line, message: @escaping () -> String) {
     #if DEBUG || TRACELOG_ENABLE || os(Linux)
         assert(LogLevel.rawTraceLevels.contains(level), "Trace levels are in the range of \(LogLevel.rawTraceLevels)")
         
