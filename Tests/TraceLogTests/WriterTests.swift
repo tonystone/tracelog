e5fb3f561c93577ac8c2e15f4a1ab2ceaaacff0e
Tony Stone <hatter24@gmail.com>
2018-12-18 09:46:20 -0800

    Add WriterTests.swift to test default implementations.

---
+++ Tests/TraceLogTests/WriterTests.swift
@@ -0,0 +1,41 @@
+///
+///  WriterTests.swift
+///
+///  Copyright 2018 Tony Stone
+///
+///  Licensed under the Apache License, Version 2.0 (the "License");
+///  you may not use this file except in compliance with the License.
+///  You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+///  Unless required by applicable law or agreed to in writing, software
+///  distributed under the License is distributed on an "AS IS" BASIS,
+///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///  See the License for the specific language governing permissions and
+///  limitations under the License.
+///
+///  Created by Tony Stone on 12/17/18.
+///
+import XCTest
+import Dispatch
+
+@testable import TraceLog
+
+/// Test to test the Writer protocol and its default implementation.
+///
+class WriterTests: XCTestCase {
+
+    /// The writers default value when no implementation is supplied, should be `true`.
+    ///
+    func testAvailableDefaultValue() {
+
+        /// Test Writer with no available implementation.
+        ///
+        class TestWriter: Writer {
+            func log(_ timestamp: Double, level: LogLevel, tag: String, message: String, runtimeContext: RuntimeContext, staticContext: StaticContext) {}
+        }
+
+        XCTAssertEqual(TestWriter().available, true)
+    }
+}
