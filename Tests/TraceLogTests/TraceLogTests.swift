7cbad8ffed33256b318d75d5f3d16711b781ae71
Tony Stone <hatter24@gmail.com>
2018-12-18 09:51:33 -0800

    Rework ValidateExpectedValuesTestWrtier to function for a broader range of cases.

--- Tests/TraceLogTests/TraceLogTests.swift
+++ Tests/TraceLogTests/TraceLogTests.swift
@@ -27,67 +27,62 @@ class TraceLogTestsSwift: XCTestCase {
     func testConfigureWithLogWriters() {
         let testMessage = "TraceLog Configured using: {\n\tglobal: {\n\n\t\tALL = INFO\n\t}\n}"
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .info, tag: "TraceLog", message: testMessage, testFileFunction: false)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .info, tag: "TraceLog", message: testMessage)
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "INFO"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "INFO"])
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testConfigureWithLogWritersAndEnvironment() {
-
         let testMessage = "TraceLog Configured using: {\n\ttags: {\n\n\t\tTraceLog = TRACE4\n\t}\n\tprefixes: {\n\n\t\tNS = ERROR\n\t}\n\tglobal: {\n\n\t\tALL = TRACE4\n\t}\n}"
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .info, tag: "TraceLog", message: testMessage, testFileFunction: false)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .info, tag: "TraceLog", message: testMessage)
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "TRACE4",
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "TRACE4",
                                                                     "LOG_PREFIX_NS": "ERROR",
                                                                     "LOG_TAG_TraceLog": "TRACE4"])
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testConfigureWithLogWritersAndEnvironmentGlobalInvalidLogLevel() {
+        let testEntries: [ValidateExpectedValuesTestWriter.ExpectedLogEntry] = [
+            (timestamp: nil, level: .info,    tag: "TraceLog", message: "TraceLog Configured using: {\n\tglobal: {\n\n\t\tALL = INFO\n\t}\n}", runtimeContext: nil, staticContext: nil),
+            (timestamp: nil, level: .warning, tag: "TraceLog", message: "Variable \'LOG_ALL\' has an invalid logLevel of \'TRACE5\'. \'LOG_ALL\' will be set to INFO.", runtimeContext: nil, staticContext: nil)
+        ]
 
-        let testMessage = "Variable \'LOG_ALL\' has an invalid logLevel of \'TRACE5\'. \'LOG_ALL\' will be set to INFO."
+        let testWriter = ValidateExpectedValuesTestWriter(expected: testEntries)
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .warning, tag: "TraceLog", message: testMessage, testFileFunction: false)
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "TRACE5"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "TRACE5"])
-
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testConfigureWithLogWritersAndEnvironmentPrefixInvalidLogLevel() {
+        let testEntries: [ValidateExpectedValuesTestWriter.ExpectedLogEntry] = [
+            (timestamp: nil, level: .info,    tag: "TraceLog", message: "TraceLog Configured using: {\n\tglobal: {\n\n\t\tALL = INFO\n\t}\n}", runtimeContext: nil, staticContext: nil),
+            (timestamp: nil, level: .warning, tag: "TraceLog", message: "Variable \'LOG_PREFIX_NS\' has an invalid logLevel of \'TRACE5\'. \'LOG_PREFIX_NS\' will NOT be set.", runtimeContext: nil, staticContext: nil)
+        ]
 
-        let testMessage = "Variable \'LOG_PREFIX_NS\' has an invalid logLevel of \'TRACE5\'. \'LOG_PREFIX_NS\' will NOT be set."
+        let testWriter = ValidateExpectedValuesTestWriter(expected: testEntries)
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .warning, tag: "TraceLog", message: testMessage, testFileFunction: false)
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_PREFIX_NS": "TRACE5"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_PREFIX_NS": "TRACE5"])
-
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testConfigureWithLogWritersAndEnvironmentTagInvalidLogLevel() {
+        let testEntries: [ValidateExpectedValuesTestWriter.ExpectedLogEntry] = [
+            (timestamp: nil, level: .info,    tag: "TraceLog", message: "TraceLog Configured using: {\n\tglobal: {\n\n\t\tALL = INFO\n\t}\n}", runtimeContext: nil, staticContext: nil),
+            (timestamp: nil, level: .warning, tag: "TraceLog", message: "Variable \'LOG_TAG_TRACELOG\' has an invalid logLevel of \'TRACE5\'. \'LOG_TAG_TRACELOG\' will NOT be set.", runtimeContext: nil, staticContext: nil)
+        ]
 
-        let testMessage = "Variable \'LOG_TAG_TRACELOG\' has an invalid logLevel of \'TRACE5\'. \'LOG_TAG_TRACELOG\' will NOT be set."
+        let testWriter = ValidateExpectedValuesTestWriter(expected: testEntries)
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .warning, tag: "TraceLog", message: testMessage, testFileFunction: false)
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_TAG_TraceLog": "TRACE5"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_TAG_TraceLog": "TRACE5"])
-
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+         self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     // MARK: ConcurrencyMode tests
@@ -191,115 +186,99 @@ class TraceLogTestsSwift: XCTestCase {
     // MARK: - Logging Methods
 
     func testLogError() {
-        let testMessage = "Swift: " + #function
+        let testMessage = "Swift: \(#function)"
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .error, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .error, tag: self.testTag, message: testMessage)
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "ERROR"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "ERROR"])
 
         logError(testTag) { testMessage }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testLogWarning() {
-        let testMessage = "Swift: " + #function
+        let testMessage = "Swift: \(#function)"
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .warning, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .warning, tag: self.testTag, message: testMessage)
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "WARNING"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "WARNING"])
 
         logWarning(testTag) { testMessage }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testLogInfo() {
         let testMessage = "Swift: " + #function
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .info, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .info, tag: testTag, message: testMessage, filterTags: ["TraceLog"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "INFO"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "INFO"])
 
-        logInfo(testTag) { testMessage }
+        logInfo(testTag) { "Swift: \(#function)" }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testLogTrace() {
         let testMessage = "Swift: " + #function
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .trace1, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .trace1, tag: testTag, message: testMessage, filterTags: ["TraceLog"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "TRACE1"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "TRACE1"])
 
         logTrace(testTag) { testMessage }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testLogTrace1() {
         let testMessage = "Swift: " + #function
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .trace1, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .trace1, tag: testTag, message: testMessage, filterTags: ["TraceLog"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "TRACE1"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "TRACE1"])
 
         logTrace(testTag, level: 1) { testMessage }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testLogTrace2() {
         let testMessage = "Swift: " + #function
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .trace2, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .trace2, tag: testTag, message: testMessage, filterTags: ["TraceLog"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "TRACE2"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "TRACE2"])
 
         logTrace(testTag, level: 2) { testMessage }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testLogTrace3() {
         let testMessage = "Swift: " + #function
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .trace3, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .trace3, tag: testTag, message: testMessage, filterTags: ["TraceLog"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "TRACE3"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "TRACE3"])
 
         logTrace(testTag, level: 3) { testMessage }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     func testLogTrace4() {
         let testMessage = "Swift: " + #function
 
-        let expectedValues = ValidateExpectedValuesTestWriter(expectation: self.expectation(description: testMessage), level: .trace4, tag: testTag, message: testMessage)
+        let testWriter = ValidateExpectedValuesTestWriter(level: .trace4, tag: testTag, message: testMessage, filterTags: ["TraceLog"])
 
-        TraceLog.configure(writers: [expectedValues], environment: ["LOG_ALL": "TRACE4"])
+        TraceLog.configure(writers: [testWriter], environment: ["LOG_ALL": "TRACE4"])
 
         logTrace(testTag, level: 4) { testMessage }
 
-        self.waitForExpectations(timeout: 2) { error in
-            XCTAssertNil(error)
-        }
+        self.wait(for: [testWriter.expectation], timeout: 2)
     }
 
     // MARK: - Logging Methods when level below log level.
