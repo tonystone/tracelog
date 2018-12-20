293384e7253677b45c0427d812d3407593b9caad
Tony Stone <hatter24@gmail.com>
2018-12-20 14:15:31 -0800

    Adding `swift test --generate-linuxmain` (SPM) generated LinuxMain.swift and XCtestManifest.swift files.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,32 +1,10 @@
-/// build-tools: auto-generated
-
-#if os(Linux) || os(FreeBSD)
-
 import XCTest
 
-@testable import TraceLogTests
-@testable import TraceLogTestHarnessTests
+import TraceLogTestHarnessTests
+import TraceLogTests
 
-XCTMain([
-   testCase(TraceLogPerformanceTestsSwift.allTests),
-   testCase(TraceLogTestsSwift.allTests),
-   testCase(EnvironmentTests.allTests),
-   testCase(ConfigurationTests.allTests),
-   testCase(TestHarnessTests.allTests),
-   testCase(TestUtilitiesTests.allTests),
-   testCase(AnyReaderTests.allTests),
-   testCase(TestUtilitiesTests.allTests),
-   testCase(ConsoleWriterTests.allTests),
-   testCase(FileWriterTests.allTests),
-   testCase(FileWriterInternalsTests.allTests),
-   testCase(TraceLogWithFileWriterTests.allTests),
-   testCase(MutexTests.allTests),
-   testCase(BlockTimerTests.allTests),
-   testCase(AsyncWriterProxyTests.allTests),
-   testCase(SyncWriterProxyTests.allTests),
-   testCase(RuntimeContextTests.allTests),
-   testCase(StaticContextTests.allTests),
-   testCase(WriterTests.allTests),
-])
+var tests = [XCTestCaseEntry]()
+tests += TraceLogTestHarnessTests.__allTests()
+tests += TraceLogTests.__allTests()
 
-#endif
+XCTMain(tests)
---
+++ Tests/TraceLogTests/XCTestManifests.swift
@@ -0,0 +1,202 @@
+import XCTest
+
+extension AsyncWriterProxyTests {
+    static let __allTests = [
+        ("testAvailableNoBuffering", testAvailableNoBuffering),
+        ("testAvailableWithDropHeadBuffering", testAvailableWithDropHeadBuffering),
+        ("testAvailableWithDropTailBuffering", testAvailableWithDropTailBuffering),
+        ("testAvailableWithExpandBuffering", testAvailableWithExpandBuffering),
+        ("testLog", testLog),
+        ("testLogWithBufferingAndDropHeadBufferLimitOverflow", testLogWithBufferingAndDropHeadBufferLimitOverflow),
+        ("testLogWithBufferingAndDropTailBufferLimitOverflow", testLogWithBufferingAndDropTailBufferLimitOverflow),
+        ("testLogWithBufferingWriterAvailable", testLogWithBufferingWriterAvailable),
+        ("testLogWithBufferingWriterDelayedAvailability", testLogWithBufferingWriterDelayedAvailability),
+        ("testLogWithBufferingWriterUnavailable", testLogWithBufferingWriterUnavailable),
+    ]
+}
+
+extension BlockTimerTests {
+    static let __allTests = [
+        ("testDealloc", testDealloc),
+        ("testRepeatFire", testRepeatFire),
+        ("testResumeWhenResumed", testResumeWhenResumed),
+        ("testSuspendResume", testSuspendResume),
+        ("testSuspendWhenSuspended", testSuspendWhenSuspended),
+    ]
+}
+
+extension ConfigurationTests {
+    static let __allTests = [
+        ("testInit", testInit),
+        ("testLoad_Prefixes", testLoad_Prefixes),
+        ("testLoad_Tags", testLoad_Tags),
+        ("testLogLevel_All_Default", testLogLevel_All_Default),
+        ("testLogLevel_All_Set", testLogLevel_All_Set),
+        ("testLogLevel_All_Set_Error", testLogLevel_All_Set_Error),
+        ("testLogLevel_All_Set_Info", testLogLevel_All_Set_Info),
+        ("testLogLevel_All_Set_InvalidLevel", testLogLevel_All_Set_InvalidLevel),
+        ("testLogLevel_All_Set_Off", testLogLevel_All_Set_Off),
+        ("testLogLevel_All_Set_Trace1", testLogLevel_All_Set_Trace1),
+        ("testLogLevel_All_Set_Trace2", testLogLevel_All_Set_Trace2),
+        ("testLogLevel_All_Set_Trace3", testLogLevel_All_Set_Trace3),
+        ("testLogLevel_All_Set_Trace4", testLogLevel_All_Set_Trace4),
+        ("testLogLevel_All_Set_Warning", testLogLevel_All_Set_Warning),
+        ("testLogLevel_Prefix", testLogLevel_Prefix),
+        ("testLogLevel_Tag", testLogLevel_Tag),
+    ]
+}
+
+extension ConsoleWriterTests {
+    static let __allTests = [
+        ("testLogError", testLogError),
+        ("testLogInfo", testLogInfo),
+        ("testLogTrace1", testLogTrace1),
+        ("testLogTrace2", testLogTrace2),
+        ("testLogTrace3", testLogTrace3),
+        ("testLogTrace4", testLogTrace4),
+        ("testLogWarning", testLogWarning),
+    ]
+}
+
+extension EnvironmentTests {
+    static let __allTests = [
+        ("testInit", testInit),
+        ("testInit_CollectionType", testInit_CollectionType),
+        ("testInit_DictionaryLiteral", testInit_DictionaryLiteral),
+        ("testSubscript", testSubscript),
+    ]
+}
+
+extension FileWriterInternalsTests {
+    static let __allTests = [
+        ("testOpen", testOpen),
+        ("testOpenCreatingDirectory", testOpenCreatingDirectory),
+        ("testOpenFailure", testOpenFailure),
+        ("testRotate", testRotate),
+        ("testRotateFailure", testRotateFailure),
+    ]
+}
+
+extension FileWriterTests {
+    static let __allTests = [
+        ("testErrorCreateFailedDescription", testErrorCreateFailedDescription),
+        ("testErrorFileDoesNotExistDescription", testErrorFileDoesNotExistDescription),
+        ("testLogError", testLogError),
+        ("testLogInfo", testLogInfo),
+        ("testLogTrace1", testLogTrace1),
+        ("testLogTrace2", testLogTrace2),
+        ("testLogTrace3", testLogTrace3),
+        ("testLogTrace4", testLogTrace4),
+        ("testLogWarning", testLogWarning),
+        ("testRotationOnInit", testRotationOnInit),
+        ("testRotationOnWrite", testRotationOnWrite),
+    ]
+}
+
+extension MutexTests {
+    static let __allTests = [
+        ("testLockBlocked", testLockBlocked),
+        ("testLockUnlock", testLockUnlock),
+        ("testLockUnlockNonRecursiveBlocked", testLockUnlockNonRecursiveBlocked),
+        ("testLockUnlockRecursive", testLockUnlockRecursive),
+        ("testTryLockBlocked", testTryLockBlocked),
+    ]
+}
+
+extension RuntimeContextTests {
+    static let __allTests = [
+        ("testDescription", testDescription),
+    ]
+}
+
+extension StaticContextTests {
+    static let __allTests = [
+        ("testDescription", testDescription),
+    ]
+}
+
+extension SyncWriterProxyTests {
+    static let __allTests = [
+        ("testAvailable", testAvailable),
+    ]
+}
+
+extension TraceLogPerformanceTestsSwift {
+    static let __allTests = [
+        ("testLogErrorPerformance_NullWriter", testLogErrorPerformance_NullWriter),
+        ("testLogTrace4Performance_NullWriter", testLogTrace4Performance_NullWriter),
+    ]
+}
+
+extension TraceLogTestsSwift {
+    static let __allTests = [
+        ("testConfigureWithLogWriters", testConfigureWithLogWriters),
+        ("testConfigureWithLogWritersAndEnvironment", testConfigureWithLogWritersAndEnvironment),
+        ("testConfigureWithLogWritersAndEnvironmentGlobalInvalidLogLevel", testConfigureWithLogWritersAndEnvironmentGlobalInvalidLogLevel),
+        ("testConfigureWithLogWritersAndEnvironmentPrefixInvalidLogLevel", testConfigureWithLogWritersAndEnvironmentPrefixInvalidLogLevel),
+        ("testConfigureWithLogWritersAndEnvironmentTagInvalidLogLevel", testConfigureWithLogWritersAndEnvironmentTagInvalidLogLevel),
+        ("testConfigureWithNoArgs", testConfigureWithNoArgs),
+        ("testLogError", testLogError),
+        ("testLogErrorWhileOff", testLogErrorWhileOff),
+        ("testLogInfo", testLogInfo),
+        ("testLogInfoWhileOff", testLogInfoWhileOff),
+        ("testLogTrace", testLogTrace),
+        ("testLogTrace1", testLogTrace1),
+        ("testLogTrace1WhileOff", testLogTrace1WhileOff),
+        ("testLogTrace2", testLogTrace2),
+        ("testLogTrace2WhileOff", testLogTrace2WhileOff),
+        ("testLogTrace3", testLogTrace3),
+        ("testLogTrace3WhileOff", testLogTrace3WhileOff),
+        ("testLogTrace4", testLogTrace4),
+        ("testLogTrace4WhileOff", testLogTrace4WhileOff),
+        ("testLogTraceWhileOff", testLogTraceWhileOff),
+        ("testLogWarning", testLogWarning),
+        ("testLogWarningWhileOff", testLogWarningWhileOff),
+        ("testModeAsyncIsDifferentThread", testModeAsyncIsDifferentThread),
+        ("testModeDirectIsSameThread", testModeDirectIsSameThread),
+        ("testModeSyncBlocks", testModeSyncBlocks),
+        ("testNoDeadLockAsyncMode", testNoDeadLockAsyncMode),
+        ("testNoDeadLockDirectMode", testNoDeadLockDirectMode),
+        ("testNoDeadLockSyncMode", testNoDeadLockSyncMode),
+    ]
+}
+
+extension TraceLogWithFileWriterTests {
+    static let __allTests = [
+        ("testLogError", testLogError),
+        ("testLogInfo", testLogInfo),
+        ("testLogTrace1", testLogTrace1),
+        ("testLogTrace2", testLogTrace2),
+        ("testLogTrace3", testLogTrace3),
+        ("testLogTrace4", testLogTrace4),
+        ("testLogWarning", testLogWarning),
+    ]
+}
+
+extension WriterTests {
+    static let __allTests = [
+        ("testAvailableDefaultValue", testAvailableDefaultValue),
+    ]
+}
+
+#if !os(macOS)
+public func __allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(AsyncWriterProxyTests.__allTests),
+        testCase(BlockTimerTests.__allTests),
+        testCase(ConfigurationTests.__allTests),
+        testCase(ConsoleWriterTests.__allTests),
+        testCase(EnvironmentTests.__allTests),
+        testCase(FileWriterInternalsTests.__allTests),
+        testCase(FileWriterTests.__allTests),
+        testCase(MutexTests.__allTests),
+        testCase(RuntimeContextTests.__allTests),
+        testCase(StaticContextTests.__allTests),
+        testCase(SyncWriterProxyTests.__allTests),
+        testCase(TraceLogPerformanceTestsSwift.__allTests),
+        testCase(TraceLogTestsSwift.__allTests),
+        testCase(TraceLogWithFileWriterTests.__allTests),
+        testCase(WriterTests.__allTests),
+    ]
+}
+#endif
