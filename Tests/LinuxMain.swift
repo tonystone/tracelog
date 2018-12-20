15064c264df2b45bd4e84da8c873e94e08951871
Tony Stone <hatter24@gmail.com>
2018-12-20 11:28:09 -0800

    Add new tests to LinuxMain.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -13,13 +13,20 @@ XCTMain([
    testCase(EnvironmentTests.allTests),
    testCase(ConfigurationTests.allTests),
    testCase(TestHarnessTests.allTests),
+   testCase(TestUtilitiesTests.allTests),
    testCase(AnyReaderTests.allTests),
    testCase(TestUtilitiesTests.allTests),
    testCase(ConsoleWriterTests.allTests),
    testCase(FileWriterTests.allTests),
    testCase(FileWriterInternalsTests.allTests),
    testCase(TraceLogWithFileWriterTests.allTests),
-   testCase(MutexTests.allTests)
+   testCase(MutexTests.allTests),
+   testCase(BlockTimerTests.allTests),
+   testCase(AsyncWriterProxyTests.allTests),
+   testCase(SyncWriterProxyTests.allTests),
+   testCase(RuntimeContextTests.allTests),
+   testCase(StaticContextTests.allTests),
+   testCase(WriterTests.allTests),
 ])
 
 #endif
