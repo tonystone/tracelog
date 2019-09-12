6ef08058771064bd6031d6c7372c7cf3beae4671
Tony Stone <hatter24@gmail.com>
2019-09-12 13:42:56 -0500

    Adding documentation note for fixed strategy concerning iOS protected data feature.

--- Sources/TraceLog/Writers & Formatters/FileWriter.swift
+++ Sources/TraceLog/Writers & Formatters/FileWriter.swift
@@ -191,6 +191,11 @@ extension FileWriter {
         ///         with this option, TraceLog will continue
         ///         to append to the file name specified.
         ///
+        /// - Note: On the iOS platform, this strategy will monitor protected data availability
+        ///         and if you use the `ConcurrencyMode.async` mode with the
+        ///         `AsyncConcurrencyModeOption.buffer(writeInterval:strategy:)` the FileWriter will
+        ///         buffer when protected data is not available.
+        ///
         /// - Remark: Once Swift Evolution [SE-0155](https://github.com/apple/swift-evolution/blob/master/proposals/0155-normalize-enum-case-representation.md) is implemented
         ///           this will func will be changed to a case in the enum with default values.  We must
         ///           use a func now to work around the lack of defaults on enums.
